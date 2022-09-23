f7f5d494dd05c2e7c8349cc2c607b10f09831174
Rafael Pereira <rafaelhpmg@gmail.com>
2022-09-23 17:18:28 -0300

    :rocket: Lista 1 de exercicios finalizada

--- exerciciosAcademia/PrimeiroApp/PrimeiroApp/Program.cs
+++ exerciciosAcademia/PrimeiroApp/PrimeiroApp/Program.cs
@@ -40,8 +40,8 @@
             // EXERCICIO 4
             //string dia = DateTime.Now.ToString("dd");
             //string mes = DateTime.Now.ToString("MM");
-            //string ano = DateTime.Now.ToString("yyyy");
-            //string anoFull = DateTime.Now.ToString("yy");
+            //string ano = DateTime.Now.ToString("yy");
+            //string anoFull = DateTime.Now.ToString("yyyy");
             //string data = ano + "/" + mes + "/" + dia;
             //string dataFull = anoFull + "/" + mes + "/" + dia;
             //Console.WriteLine(data);
@@ -155,11 +155,113 @@
             //}
 
             // EXERCICIO 11
+            //float brancos, nulos, validos;
+            //Console.WriteLine("Insira a quantidade de votos em branco");
+            //brancos = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Insira a quantidade de votos em nulo");
+            //nulos = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Insira a quantidade de votos validos");
+            //validos = float.Parse(Console.ReadLine());
+            //float total = brancos + nulos + validos;
+
+            //Console.WriteLine("Porcentagem de votos em branco >> "+(brancos/total * 100) + "%");
+            //Console.WriteLine("Porcentagem de votos nulos >> " + (nulos / total * 100)  + "%");
+            //Console.WriteLine("Porcentagem de votos validos >> " + (validos / total * 100)  + "%");
 
             // EXERCICIO 12
+            //double combustivel = 6.90;
+            //Console.WriteLine("Informe a quilometragem do inicio do dia");
+            //float odometro1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe a quilometragem do fim do dia");
+            //float odometro2 = float.Parse(Console.ReadLine());
+            //float quilometragem = odometro2 - odometro1;
+            //Console.WriteLine("Infome a quantidade de combustivel gasta em Litros");
+            //float gastoCombustivel = float.Parse(Console.ReadLine());
+            //float mediaConsumo = quilometragem / gastoCombustivel;
+            //Console.WriteLine("Informe o valor total recebido");
+            //float recebido = float.Parse(Console.ReadLine());
+            //double lucro = recebido - gastoCombustivel * combustivel;
+            //Console.WriteLine("Quilometragem total >> "+ quilometragem);
+            //Console.WriteLine("Media de consumo >> " + mediaConsumo);
+            //Console.WriteLine("Lucro total >> " + lucro);
 
             // EXERCICIO 13
+            //Console.WriteLine("Informe o valor do salario minimo");
+            //float salarioMin = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe o preço de custo das bicicletas");
+            //float bicicletaPreco = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe a quantidade de bicicletas vendidas pelo vendedor");
+            //int qtdBicicletas = int.Parse(Console.ReadLine());
+            //double comissao = (bicicletaPreco*0.15)*qtdBicicletas;
+            //double salarioFinal = salarioMin * 2 + comissao;
+            //Console.WriteLine("O salário do vendedor é de >> "+salarioFinal);
 
+            // EXERCICIOS DA AULA \/
+
+            //double num1, num2, resultado;
+            //int op;
+            //Console.WriteLine("Informe uma operação: \n1.Adição\n2.Subtração\n3.Multiplicação\n4.Divisão\n5.Potencia\n0.Fechar\nEscolha: ");
+            //op = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("Informe dois numeros");
+            //    num1 = double.Parse(Console.ReadLine());
+            //    num2 = double.Parse(Console.ReadLine());
+
+            //    switch (op)
+            //    {
+            //        case 1:
+            //            resultado = num1 + num2;
+            //            Console.WriteLine(resultado);
+            //            return;
+            //        case 2:
+            //            resultado = num1 - num2;
+            //            Console.WriteLine(resultado);
+            //            return;
+            //        case 3:
+            //            resultado = num1 * num2;
+            //            Console.WriteLine(resultado);
+            //            return;
+
+            //        case 4:
+            //            resultado = num1 / num2;
+            //            Console.WriteLine(resultado);
+            //            return;
+
+            //        case 5:
+            //            resultado = Math.Pow(num1, num2);
+            //            Console.WriteLine(resultado);
+            //            return;
+            //        default:
+            //            break;
+            //    }
+
+            //int lado1, lado2, lado3;
+
+            //Console.WriteLine("Informe um lado");
+            //lado1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe um lado");
+            //lado2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe um lado");
+            //lado3 = int.Parse(Console.ReadLine());
+
+            //if(lado1 < lado2+lado3 && lado2 < lado1 + lado2 && lado3 < lado1 + lado2)
+            //{
+            //    if(lado1 == lado2 && lado1 == lado3)
+            //    {
+            //        Console.WriteLine("Triangulo equilatero");
+            //    } else if(lado1 == lado2 || lado2 == lado3 || lado1 == lado3) 
+            //    {
+            //        Console.WriteLine("Triangulo isosceles");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Triangulo escaleno");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não é um triangulo");
+            //}
 
         }
     }
