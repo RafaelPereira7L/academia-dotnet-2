f7568655fdd11bda29f01258925335d5e8e3a1d4
Rafael Pereira <rafaelhpmg@gmail.com>
2022-09-27 16:18:41 -0300

    :sparkles: Loops

---
+++ exerciciosAcademia/laçosDeRepetição/laçosDeRepetição/Program.cs
@@ -0,0 +1,13 @@
+﻿int contador=0;
+Console.WriteLine("Insira um numero");
+int num = int.Parse(Console.ReadLine());
+while (num >= 0)
+{
+    Console.WriteLine("Insira um numero");
+    num = int.Parse(Console.ReadLine());
+    if (num % 2 == 0)
+    {
+        contador++;
+        Console.WriteLine(contador+" numeros pares");
+    }
+}
