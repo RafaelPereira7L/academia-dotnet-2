f635697b1d034b32139430933a6202a99c132176
Rafael Pereira <rafaelhpmg@gmail.com>
2022-09-30 17:43:49 -0300

    :sparkles: Vetores

--- exerciciosAcademia/vetores/vetores/Program.cs
+++ exerciciosAcademia/vetores/vetores/Program.cs
@@ -65,3 +65,254 @@ for (i = 0; i < 10; i++)
     }
 }
 Console.WriteLine("Maior valor "+maior);*/
+
+
+/* EXERCICIO 1
+
+int[] vetor = new int[10];
+int pares = 0, impares = 0;
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+    if (vetor[i] % 2 == 0)
+    {
+        pares++;
+    }
+    else
+    {
+        impares++;
+    }
+}
+
+Console.WriteLine("Quantidade de numeros pares "+pares);
+Console.WriteLine("Quantidade de numeros impares "+impares);*/
+
+// EXERCICIO 2
+/*int[] vetor = new int[20];
+int[] vetor1 = new int[20];
+int[] somaVetores = new int[20];
+
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < vetor1.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor1[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < somaVetores.Length; i++)
+{
+    somaVetores[i] = vetor[i] + vetor1[i];
+    Console.WriteLine(somaVetores[i]);
+}*/
+
+// EXERCICIO 3
+/*int[] vetor = new int[5];
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+    int qtdDivisoes = 0;
+    int counter = 1;
+    while (counter < 8)
+    { 
+        if (vetor[i] % counter == 0) qtdDivisoes++;
+        counter++;
+    }
+    if (qtdDivisoes > 2)
+    {
+        Console.WriteLine("O numero "+vetor[i]+" não é primo");
+    }
+    else
+    {
+        Console.WriteLine("O numero "+vetor[i]+" é primo!");
+    }
+}*/
+
+// EXERCICIO 4
+/*int[] vetor = new int[10];
+int[] vetor1 = new int[10];
+int[] somaVetores = new int[10];
+
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < vetor1.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor1[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < somaVetores.Length; i++)
+{
+    somaVetores[i] = vetor[i] * vetor1[i];
+}
+
+for (int i = 0; i < somaVetores.Length; i++)
+{
+    Console.Write(somaVetores[i]+" | ");
+}*/
+
+// EXERCICIO 5
+/*
+int[] vetor = new int[5];
+int menorValor = 100000;
+int indice = 0;
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+
+    if (menorValor > vetor[i])
+    {
+        menorValor = vetor[i];
+        indice = i;
+    }
+}
+Console.WriteLine("Menor valor => "+menorValor+" índice => "+indice);*/
+
+// EXERCICIO 6
+/*int[] vetor = new int[11];
+
+for (int i = 1; i < vetor.Length; i++)
+{
+    int index = vetor.Length - i;
+    Console.WriteLine("Informe um numero");
+    vetor[index] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i]+" | ");
+}*/
+
+// EXERCICIO 7
+/*int[] vetor = new int[10];
+int[] vetor2 = new int[10];
+
+for (int i = 1; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+    int number = vetor[i];
+    if (number % 2 == 0)
+    {
+        vetor2[i] = number;
+    }
+    else
+    {
+        vetor2[(vetor2.Length-i)] = number;
+    }
+}
+
+for (int i = 1; i < vetor2.Length; i++)
+{
+    Console.Write(vetor2[i]+" | ");
+}*/
+
+// EXERCICIO 8 
+/*float[] vetor = new float[5];
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = float.Parse(Console.ReadLine());
+}
+
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    if (vetor[i] < 0)
+    {
+        Console.Write(i +" | ");        
+    }
+    else
+    {
+        Console.Write(vetor[i]+" | ");        
+    }
+}*/
+
+// EXERCICIO 9
+/*int[] vetor = new int[10];
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+Array.Sort(vetor);
+foreach(int num in vetor)
+{
+    Console.Write(num);
+}*/
+
+// EXERCICIO 10
+/*int[] vetor = new int[10];
+int[] vetor2 = new int[10];
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    if (vetor[i] != 0)
+    {
+        vetor2[i] = vetor[i];
+    }
+    else
+    {
+        vetor2[i] = 2;
+    }
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine(vetor[i]+" | ");
+    Console.WriteLine(vetor2[i]+" | ");
+}*/
+
+// EXERCICIO 11
+/*int[] vetor = new int[10];
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+
+Array.Reverse(vetor);
+foreach(int num in vetor)
+{
+    Console.Write(num+" | ");
+}*/
+
+// EXERCICIO 12 
+
+int[] vetor = new int[10];
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine("Informe um numero");
+    vetor[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Informe 0 numero");
+numero = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    
+}
